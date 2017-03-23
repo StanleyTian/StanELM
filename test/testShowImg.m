40aa0adef5168a42326ecd109ff61207635b2725
史丹利复合田 <b20130525@xs.ustb.edu.cn>
2017-03-23 19:14:03 +0800

    增加了lbp函数

--- test/testShowImg.m
+++ test/testShowImg.m
@@ -1,4 +1,11 @@
 imgData = X(6,:);
 img = array_to_color_image(imgData,96,96);
-B = imrotate(img,270);%stl-10的数据需要顺时针旋转90°才为正
-imshow(B);
+colorImage = imrotate(img,270);%stl-10的数据需要顺时针旋转90°才为正
+subplot(1,3,1);
+imshow(colorImage);
+grayImage = rgb2gray(colorImage);
+subplot(1,3,2);
+imshow(grayImage);
+re = lbp(grayImage);
+subplot(1,3,3);
+plot(re);
